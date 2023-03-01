f639953263c981ef18cbca3cde1f7ec33a2d8436
tarikwaleed <takizee53@gmail.com>
2023-03-01 21:21:46 +0200

    minizing the code for form validation

--- src/components/login_form/login_form.jsx
+++ src/components/login_form/login_form.jsx
@@ -23,16 +23,12 @@ const LoginForm = () => {
                         aria-describedby="helpId"
                         placeholder="Enter email"
                         {...register("email", {
-                            required: true,
-                            pattern: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/
+                            required: "Email is required",
+                            pattern: { value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/, message: "Email is not valid" }
                         })}
                     />
-                    {errors.email && errors.email.type === "required" && (
-                        <p className="text-danger">Email is required.</p>
-                    )}
-                    {errors.email && errors.email.type === "pattern" && (
-                        <p className="text-danger">Email is not valid.</p>
-                    )}
+                    {errors.email && <p className="text-danger">{errors.email.message}</p>}
+
                 </div>
                 <div className="mb-3 ">
                     <label htmlFor="email-input" className="form-label">
@@ -45,16 +41,16 @@ const LoginForm = () => {
                         id=""
                         aria-describedby="helpId"
                         placeholder="Enter password"
-                        {...register("password", { required: true, minLength: 6 })}
+                        {...register("password", {
+                            required: "Password is required",
+                            minLength:
+                            {
+                                value: 6,
+                                message: "Password should be at least 6 characters"
+                            }
+                        })}
                     />
-                    {errors.password && errors.password.type === "required" && (
-                        <p className="text-danger">Password is required.</p>
-                    )}
-                    {errors.password && errors.password.type === "minLength" && (
-                        <p className="text-danger">
-                            Password should be at-least 6 characters.
-                        </p>
-                    )}
+                    {errors.password && <p className="text-danger">{errors.password.message}</p>}
                 </div>
                 <div class="d-grid gap-2">
                     <button type="submit" name="" id="" class="btn btn-primary">Login</button>
